360f0accfe26b7fd3c2a6508c065b53ccb6e429c
John Bachman <bachmanjohn@gmail.com>
2018-07-09 12:38:55 -0500

    Makefile target for toy_example

--- Makefile
+++ Makefile
@@ -3,16 +3,18 @@ DATA := data
 NET := networks
 #DEPLOY := ~/Dropbox/DARPA\ projects/papers/INDRA\ paper\ 2/figure_panels/
 
-all: preprocessing
+all: preprocessing toy_example
 
 preprocessing: \
         $(OUTPUT)/pc_digraph.pkl \
         $(OUTPUT)/pc_digraph_small.pkl
 
-$(BUILD)/pc_digraph.pkl: \
-        $(OUTPUT)/PathwayCommons9.All.hgnc.txt \
+$(OUTPUT)/pc_digraph.pkl: \
+        $(DATA)/PathwayCommons9.All.hgnc.txt \
         $(DATA)/prior_genes.txt
 	python preprocess_pc.py
 
+toy_example: $(OUTPUT)/g.pdf
 
-
+$(OUTPUT)/g.pdf: toy_example.py
+	python toy_example.py $(OUTPUT)
--- toy_example.py
+++ toy_example.py
@@ -1,3 +1,5 @@
+import sys
+from os.path import join
 import itertools
 import networkx as nx
 from paths_graph import PathsGraph, CombinedPathsGraph, CFPG, \
@@ -43,7 +45,7 @@ def draw(g, filename):
     ag.draw(filename, prog='dot')
 
 
-def draw_reachset(g, level, direction, depth):
+def draw_reachset(g, level, direction, depth, output_dir):
     if direction not in ('forward', 'backward'):
         raise ValueError("direction must be 'forward' or 'backward'")
     edges = []
@@ -62,71 +64,58 @@ def draw_reachset(g, level, direction, depth):
                 edges.append((prev_node, cur_node))
     graph = nx.DiGraph()
     graph.add_edges_from(edges)
-    draw(graph, '%s_graph.pdf' % direction)
+    draw(graph, join(output_dir, '%s_graph.pdf' % direction))
 
-# Draw G
-draw(g, 'g.pdf')
+if __name__ == '__main__':
 
-depth = 4
-source = 'S'
-target = 'T'
+    output_dir = sys.argv[1]
 
-f_level, b_level = get_reachable_sets(g, source, target, depth)
-draw_reachset(g, f_level, 'forward', depth)
-draw_reachset(g, b_level, 'backward', depth)
+    # Draw G
+    draw(g, join(output_dir, 'g.pdf'))
 
-print("f_level", f_level)
-print("b_level", b_level)
+    depth = 4
+    source = 'S'
+    target = 'T'
 
-pg = PathsGraph.from_graph(g, source, target, depth)
-draw(pg.graph, 'pg_%d.pdf' % depth)
+    f_level, b_level = get_reachable_sets(g, source, target, depth)
+    draw_reachset(g, f_level, 'forward', depth, output_dir)
+    draw_reachset(g, b_level, 'backward', depth, output_dir)
 
-# Combined paths graph
-pg_list = []
-for i in range(1, 6+1):
-    pg_list.append(PathsGraph.from_graph(g, source, target, i))
-cpg = CombinedPathsGraph(pg_list)
-draw(cpg.graph, 'combined_pg.pdf')
+    print("f_level", f_level)
+    print("b_level", b_level)
 
-# Cycle-free paths graph
-cfpg = CFPG.from_pg(pg)
-# Remove the frozensets for drawing
-cfpg_edges_fixed = []
-for u, v in cfpg.graph.edges():
-    u_set = '{}' if u[2] == 0 else str(set(u[2]))
-    v_set = '{}' if v[2] == 0 else str(set(v[2]))
-    u_fixed = str((u[0], u[1], u_set))
-    v_fixed = str((v[0], v[1], v_set))
-    cfpg_edges_fixed.append((u_fixed, v_fixed))
-cfpg_fixed = nx.DiGraph()
-cfpg_fixed.add_edges_from(cfpg_edges_fixed)
-draw(cfpg_fixed, 'cfpg_%d.pdf' % depth)
+    pg = PathsGraph.from_graph(g, source, target, depth)
+    draw(pg.graph, join(output_dir, 'pg_%d.pdf' % depth))
 
-# Non-uniform sampling
-g_samp = nx.DiGraph() # Graph for testing sampling uniformly vs. non-uniformly
-g_samp.add_edges_from([
-    ('S', 'A1'), ('S', 'A2'),
-    ('A1', 'B1'),
-    ('A2', 'B2'), ('A2', 'B3'), ('A2', 'B4'), ('A2', 'B5'),
-    ('B1', 'T'),
-    ('B2', 'T'), ('B3', 'T'), ('B4', 'T'), ('B5', 'T')])
-draw(g_samp, 'g_samp.pdf')
+    # Combined paths graph
+    pg_list = []
+    for i in range(1, 6+1):
+        pg_list.append(PathsGraph.from_graph(g, source, target, i))
+    cpg = CombinedPathsGraph(pg_list)
+    draw(cpg.graph, join(output_dir, 'combined_pg.pdf'))
 
+    # Cycle-free paths graph
+    cfpg = CFPG.from_pg(pg)
+    # Remove the frozensets for drawing
+    cfpg_edges_fixed = []
+    for u, v in cfpg.graph.edges():
+        u_set = '{}' if u[2] == 0 else str(set(u[2]))
+        v_set = '{}' if v[2] == 0 else str(set(v[2]))
+        u_fixed = str((u[0], u[1], u_set))
+        v_fixed = str((v[0], v[1], v_set))
+        cfpg_edges_fixed.append((u_fixed, v_fixed))
+    cfpg_fixed = nx.DiGraph()
+    cfpg_fixed.add_edges_from(cfpg_edges_fixed)
+    draw(cfpg_fixed, join(output_dir, 'cfpg_%d.pdf' % depth))
 
-
-# S x A, B, C, D, E, ...
-# (A, B, C, D, ...) x (A, B, C, D, ...)
-# (A, A), (A, B), ...
-# A: B, C, D
-# B: A, C, D
-# (A, B): [(0, 1), (1, 2), (1, 3)] etc.
-# (B, C): [1, 3, 4]
-# Formulate the product, for each possible edge relevant level to dict (fast
-# n^2)
-# Then, intersect the dict keys with the graph edges; the edges in the
-# intersection are legit.
-# Iterate over these (worst case n^2);
-# index into dict and iterate over the node levels to add (worst case n^2 l)
-# n^2 + 2n^2 3n^2
-# sum(1, l) n^2
+    # Non-uniform sampling
+    # Graph for testing sampling uniformly vs. non-uniformly
+    g_samp = nx.DiGraph()
+    g_samp.add_edges_from([
+        ('S', 'A1'), ('S', 'A2'),
+        ('A1', 'B1'),
+        ('A2', 'B2'), ('A2', 'B3'), ('A2', 'B4'), ('A2', 'B5'),
+        ('B1', 'T'),
+        ('B2', 'T'), ('B3', 'T'), ('B4', 'T'), ('B5', 'T')])
+    draw(g_samp, join(output_dir, 'g_samp.pdf'))
 
