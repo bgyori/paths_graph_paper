1dcc50bb18638ba05e49a813f5ca03f814ccbef6
Ben Gyori <ben.gyori@gmail.com>
2018-09-20 15:58:30 -0400

    Random graph scaling script updates

--- scaling_random_graphs.py
+++ scaling_random_graphs.py
@@ -1,3 +1,9 @@
+"""The purpose of this script is to generate random graphs of a given size
+and test multiple path finding methods between a selected source and target.
+The plot of how path finding time scales with the size of the graph is then
+produced.
+"""
+
 import time
 import itertools
 import numpy as np
@@ -6,6 +12,8 @@ from matplotlib import pyplot as plt
 from paths_graph import PathsGraph, get_reachable_sets, CFPG, \
                         CombinedPathsGraph, PathsTree
 
+
+
 seed = 1
 
 def scaling_random_graphs(num_samples, min_size, max_size, edge_prob=0.5):
@@ -13,23 +21,36 @@ def scaling_random_graphs(num_samples, min_size, max_size, edge_prob=0.5):
     times_nx_paths = np.empty(data_shape)
     times_pg = np.empty(data_shape)
     times_cfpg = np.empty(data_shape)
+
+    # Iterate over number of nodes in network
     for i, num_nodes in enumerate(range(min_size, max_size+1)):
-        print(num_nodes)
+        print(f'Number of nodes in network: {num_nodes}')
+
+        # Iterate over num_samples random graphs of this size
         for j in range(num_samples):
-            print("Sample", j)
+            print(f'Sample {j}')
             # Generate a random graph
             rg = nx.erdos_renyi_graph(num_nodes, edge_prob, directed=True)
+
+            # Select two nodes as source and target
             source = 0
             target = num_nodes - 1
+
             # Time to compute all simple paths with path probabilities
             start = time.time()
-            paths = list(nx.all_simple_paths(rg, source, target))
+            paths = [tuple(p) for p in nx.all_simple_paths(rg, source, target)]
+            #paths2 = [tuple(p) for p in nx.shortest_simple_paths(rg, source, target)]
+            #assert(set(paths) == set(paths2))
+
+            # Now build a path tree from the paths and calculate probabilities
             pt = PathsTree(paths)
             path_probs = pt.path_probabilities()
+            # Save the time it took the calculate
             end = time.time()
             elapsed = end - start
             times_nx_paths[i, j] = elapsed
-            # Time to compute paths_graphs
+
+            # Time to compute paths_graphs and make combined graph
             pg_start = time.time()
             f_level, b_level = get_reachable_sets(rg, source, target, num_nodes)
             pg_list = []
@@ -38,18 +59,21 @@ def scaling_random_graphs(num_samples, min_size, max_size, edge_prob=0.5):
                                            f_level, b_level)
                 pg_list.append(pg)
             combined_pg = CombinedPathsGraph(pg_list)
+            # NOTE: no count_paths method
             total_paths = combined_pg.count_paths()
-            print("Total paths (with cycles)", total_paths)
+            print(f'Total paths (with cycles): {total_paths}')
+
             #cf_paths = combined_pg.sample_cf_paths(100000)
             pg_elapsed = time.time() - pg_start
             times_pg[i, j] = pg_elapsed
+
             # Now compute the CFPG
-            #cfpg_list = []
-            #for pg in pg_list:
-            #    cfpg = CFPG.from_pg(pg)
-            #    cfpg_list.append(cfpg)
-            #cfpg_elapsed = time.time() - pg_start
-            #times_cfpg[i, j] = cfpg_elapsed
+            cfpg_list = []
+            for pg in pg_list:
+                cfpg = CFPG.from_pg(pg)
+                cfpg_list.append(cfpg)
+            cfpg_elapsed = time.time() - pg_start
+            times_cfpg[i, j] = cfpg_elapsed
     return times_nx_paths, times_pg, times_cfpg
 
 
@@ -74,23 +98,26 @@ def prob_ascending_path(paths):
     return count_ascending / norm_factor
 
 
+'''
 # Generate a random graph
 if __name__ == '__main__':
-    num_nodes = 16
+    num_nodes = 5
     rg = nx.erdos_renyi_graph(num_nodes, 0.5, directed=True)
     source = 0
     target = num_nodes - 1
     # Time to compute all simple paths with path probabilities
     start = time.time()
-    print("Enumerating paths")
+    print('Enumerating paths')
     paths = list(nx.all_simple_paths(rg, source, target))
-    print("Done enumerating paths")
+    print('Building path tree')
     pt = PathsTree(paths)
+    print('Getting path probabilities')
     path_probs = pt.path_probabilities()
     end = time.time()
     elapsed = end - start
     # Time to compute paths_graphs
     pg_start = time.time()
+    print('Getting reachable sets')
     f_level, b_level = get_reachable_sets(rg, source, target, num_nodes)
     pg_list = []
     for length in range(1, num_nodes):
@@ -106,24 +133,28 @@ if __name__ == '__main__':
     print("Prob CF ascending (PG)", prob_ascending_path(cf_paths))
     print("NX elapsed", elapsed)
     print("PG elapsed", pg_elapsed)
+'''
 
-"""
 if __name__ == '__main__':
-    min_size = 8
-    max_size = 9
+    # Some basic parameters
+    min_size = 10
+    max_size = 13
     num_samples = 10
     lengths = range(min_size, max_size+1)
+
+    # Run the path finding methods
     nx_results, pg_results, cfpg_results = \
                     scaling_random_graphs(num_samples, min_size, max_size, 0.5)
 
+    # Plotting
     plt.ion()
     plt.figure()
     plt.errorbar(lengths, nx_results.mean(axis=1),
-                 yerr=nx_results.std(axis=1, ddof=1))
+                 yerr=nx_results.std(axis=1, ddof=1), label='NX')
     plt.errorbar(lengths, pg_results.mean(axis=1),
-                 yerr=pg_results.std(axis=1, ddof=1))
+                 yerr=pg_results.std(axis=1, ddof=1), label='PG')
     plt.errorbar(lengths, cfpg_results.mean(axis=1),
-                 yerr=cfpg_results.std(axis=1, ddof=1))
+                 yerr=cfpg_results.std(axis=1, ddof=1), label='CF')
     ax = plt.gca()
     ax.set_yscale('log')
-"""
+    plt.legend()
